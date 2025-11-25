48d0b8bb7f45b4d77751134e4bdaa2b3be591348
Angel Ysmael <angelysmael7@gmail.com>
2025-11-24 22:35:15 -0500

    Part 2

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ const student = {
         return `${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`;
     }
 };
-@@ -7,4 +7,8 @@ const student = {
-    displayInfo() {
-        return `${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`;
-    }
-};
-};
 
 console.log("Name:", student.name);
 console.log("Age:", student.age);
-console.log("Student info:", student.displayInfo)
+console.log("Student info:", student.displayInfo());
+    //part 2//
+const jsonString = JSON.stringify(student);
+console.log("JSON String:", jsonString);
+
+const studentFromJSON = JSON.parse(jsonString);
+console.log("Parsed Object:", studentFromJSON);
+console.log("Is parsed equal to original?", JSON.stringify(studentFromJSON) === JSON.stringify(student));
