6885e4ed6ad1f811a4709ea69260853308bceb2e
Angel Ysmael <angelysmael7@gmail.com>
2025-11-24 22:33:16 -0500

    Added console.log

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ const student = {
         return `${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`;
     }
 };
+@@ -7,4 +7,8 @@ const student = {
+    displayInfo() {
+        return `${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`;
+    }
+};
+};
+
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Student info:", student.displayInfo)
